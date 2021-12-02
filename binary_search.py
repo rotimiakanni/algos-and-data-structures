f0a6f9bc0e45ae94dd30b72f6731b194ece81689
Rotimi Akanni <akannirotimi@yahoo.com>
2021-12-02 19:18:53 +0100

    binary search

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(nums: list[int], target: int) -> int:
+    '''
+    Return the index of a value in a sorted array. If not found, it returns -1
+    '''
+    upper_bound = len(nums) - 1
+    lower_bound = 0
+    while lower_bound <= upper_bound:
+        mid_point = (upper_bound + lower_bound) // 2
+        if nums[mid_point] == target:
+            return mid_point
+        elif nums[mid_point] > target:
+            upper_bound = mid_point - 1
+            mid_point = (upper_bound + lower_bound) // 2
+        else:
+            lower_bound = mid_point + 1
+            mid_point = (upper_bound + lower_bound) // 2
+    return -1
